d5bac9d35cdb478a735ea8e30bb76e06e90ec7e0
Tati Dev <tatianaworkhome@gmail.com>
2025-03-08 11:02:00 -0300

    importando o hero e  a fonte AVENIR

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 const gulp =require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle : 'compressed'}))
     .pipe(gulp.dest('./dist/css'));
 }
-exports.default = styles;
 
-exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+function images(){
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
 }
+
+function fonts() {
+    return gulp.src('./assets/fonts/**/*')
+        .pipe(gulp.dest('./dist/fonts'));
+}
+
+exports.default = gulp.parallel(styles, images, fonts);
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/images/**/*', gulp.parallel(images));
+    gulp.watch('./assets/fonts/**/*', gulp.parallel(fonts));
+};
