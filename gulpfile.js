8479ec0d2d3887d17fffdd21c295e01a9ec8ea30
Tati Dev <tatianaworkhome@gmail.com>
2025-04-07 20:52:39 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp =require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
+}
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
@@ -19,10 +26,11 @@ function fonts() {
         .pipe(gulp.dest('./dist/fonts'));
 }
 
-exports.default = gulp.parallel(styles, images, fonts);
+exports.default = gulp.parallel(styles, images, fonts, scripts);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles)),
     gulp.watch('./src/images/**/*', gulp.parallel(images)),
     gulp.watch('./assets/fonts/**/*', gulp.parallel(fonts))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 };
